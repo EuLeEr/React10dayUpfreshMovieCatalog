194cd9f295ce27ffc7e978dcd904941bcbe5c664
Eugene Ermolov <eermolov@mail.ru>
2020-04-18 21:08:29 +0300

    Updates

---
+++ precache-manifest.0c9c8e46270e4ef4894e7df16abcf5fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d59d5c0d8c25edd0a101368dc7594566",
+    "url": "/React10dayUpfreshMovieCatalog/index.html"
+  },
+  {
+    "revision": "a0f307ed1387df3e3a86",
+    "url": "/React10dayUpfreshMovieCatalog/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "16cab89a464b812020d9",
+    "url": "/React10dayUpfreshMovieCatalog/static/css/main.5d937b94.chunk.css"
+  },
+  {
+    "revision": "a0f307ed1387df3e3a86",
+    "url": "/React10dayUpfreshMovieCatalog/static/js/2.79ce769d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React10dayUpfreshMovieCatalog/static/js/2.79ce769d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16cab89a464b812020d9",
+    "url": "/React10dayUpfreshMovieCatalog/static/js/main.627959a7.chunk.js"
+  },
+  {
+    "revision": "231f3ecff26e9e6dffb8",
+    "url": "/React10dayUpfreshMovieCatalog/static/js/runtime-main.f55b9715.js"
+  }
+]);
